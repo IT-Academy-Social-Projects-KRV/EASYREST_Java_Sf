169a90e3a9fe2a6ebbc0572d55cecd821f3c959e
desislavavelichkova <desivelichkova82@gmail.com>
2023-01-22 17:20:11 +0200

    refactor file

--- src/test/java/com/easyrestaurant/tests/AsAdmin_ICan_BanModerator.java
+++ src/test/java/com/easyrestaurant/tests/AsAdmin_ICan_BanModerator.java
@@ -17,22 +17,11 @@ public class AsAdmin_ICan_BanModerator {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
 
         WebDriver driver = new ChromeDriver();
-
-
-<<<<<<< HEAD:src/test/java/com/easyrestaurant/tests/AsAdmin_ICan_BanModerator.java
 		driver.manage().window().maximize();
 		String url = "http://localhost:3000";
 		driver.get(url);
 		WebElement signInButton = driver.findElement(By.xpath("//div[@id='root']/header//div[@class='UserMenu-root-109']/div/a[1]/span[@class='MuiButton-label-81']"));
 		signInButton.click();
-=======
-        driver.manage().window().maximize();
-        String url = "http://localhost:3000";
-        driver.get(url);
-
-        WebElement signInButton = driver.findElement(By.xpath("//div[@id='root']/header//div[@class='UserMenu-root-109']/div/a[1]/span[@class='MuiButton-label-81']"));
-        signInButton.click();
->>>>>>> develop:src/test/java/com/easyrestaurant/tests/AsAdmin_ICan_BanOwner.java
 
         WebElement emailElement = driver.findElement(By.name("email"));
         emailElement.sendKeys("steveadmin@test.com");
@@ -45,7 +34,6 @@ public class AsAdmin_ICan_BanModerator {
 
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
-<<<<<<< HEAD:src/test/java/com/easyrestaurant/tests/AsAdmin_ICan_BanModerator.java
 		WebElement moderatorButton = driver.findElement(By.xpath("/html/body/div/div/ul/a[3]"));
 		moderatorButton.click();
 
@@ -59,21 +47,6 @@ public class AsAdmin_ICan_BanModerator {
 		}
 		String expectedMsg = "Banned";
 		Assert.assertEquals(expectedMsg, statusMsg.getText());
-=======
-        WebElement ownerButton = driver.findElement(By.xpath("/html/body/div/div/ul/a[2]/div[2]/span"));
-        ownerButton.click();
-
-        WebElement activeOwnerButton = driver.findElement(By.xpath("/html/body/div/div/main/div[2]/table/tbody/tr[1]/td[5]/button"));
-        WebElement statusMsg = driver.findElement(By.xpath("/html/body/div/div/main/div[2]/table/tbody/tr[1]/td[4]/p"));
-
-        if (statusMsg.getText() == "Active") {
-            activeOwnerButton.click();
-            statusMsg = driver.findElement(By.xpath("/html/body/div/div/main/div[2]/table/tbody/tr[1]/td[4]/p"));
-        }
-
-        String expectedActivityMsg = "Banned";
-        Assert.assertEquals(expectedActivityMsg, statusMsg.getText());
->>>>>>> develop:src/test/java/com/easyrestaurant/tests/AsAdmin_ICan_BanOwner.java
 
         driver.close();
     }
