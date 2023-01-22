b41bc4c29d38e20aa3a21d68470965cad4ef3062
desislavavelichkova <desivelichkova82@gmail.com>
2023-01-22 17:44:10 +0200

    refactor file

--- src/test/java/com/easyrestaurant/tests/AsAdmin_ICan_UnbanModerator.java
+++ src/test/java/com/easyrestaurant/tests/AsAdmin_ICan_UnbanModerator.java
@@ -11,30 +11,16 @@ import java.util.concurrent.TimeUnit;
 
 public class AsAdmin_ICan_UnbanModerator {
 
-<<<<<<< HEAD:src/test/java/com/easyrestaurant/tests/AsAdmin_ICan_UnbanModerator.java
-	@Test
-	public void test() {
-		
-		System.setProperty("web-driver.chrome.driver",
-				"src/main/resources/chromedriver");
-		
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		String url = "http://localhost:3000";
-		driver.get(url);
-=======
     @Test
     public void test() {
 
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+        System.setProperty("web-driver.chrome.driver",
+                "src/main/resources/chromedriver");
 
         WebDriver driver = new ChromeDriver();
-
-
         driver.manage().window().maximize();
         String url = "http://localhost:3000";
         driver.get(url);
->>>>>>> develop:src/test/java/com/easyrestaurant/tests/AsAdmin_ICan_BanOwner.java
 
         WebElement signInButton = driver.findElement(By.xpath("//div[@id='root']/header//div[@class='UserMenu-root-109']/div/a[1]/span[@class='MuiButton-label-81']"));
         signInButton.click();
@@ -50,40 +36,20 @@ public class AsAdmin_ICan_UnbanModerator {
 
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
-<<<<<<< HEAD:src/test/java/com/easyrestaurant/tests/AsAdmin_ICan_UnbanModerator.java
-		WebElement moderatorButton = driver.findElement(By.xpath("/html/body/div/div/ul/a[3]/div[2]/span"));
-		moderatorButton.click();
+        WebElement moderatorButton = driver.findElement(By.xpath("/html/body/div/div/ul/a[3]/div[2]/span"));
+        moderatorButton.click();
 
-		WebElement statusMsg = driver.findElement(By.xpath("/html/body/div/div/main/div[2]/table/tbody/tr[1]/td[4]/p"));
-		WebElement statusModeratorButton = driver.findElement(By.xpath("/html/body/div/div/main/div[2]/table/tbody/tr[1]/td[5]/button"));
-
-		if (statusMsg.getText().equals("Banned")){
-			statusModeratorButton.click();
-			statusMsg = driver.findElement(By.xpath("/html/body/div/div/main/div[2]/table/tbody/tr[1]/td[4]/p"));
-		}
-
-		String expectedActivityMsg = "Active";
-		Assert.assertEquals(expectedActivityMsg, statusMsg.getText());
-
-		driver.close();
-	}
-}
-=======
-        WebElement ownerButton = driver.findElement(By.xpath("/html/body/div/div/ul/a[2]/div[2]/span"));
-        ownerButton.click();
-
-        WebElement activeOwnerButton = driver.findElement(By.xpath("/html/body/div/div/main/div[2]/table/tbody/tr[1]/td[5]/button"));
         WebElement statusMsg = driver.findElement(By.xpath("/html/body/div/div/main/div[2]/table/tbody/tr[1]/td[4]/p"));
+        WebElement statusModeratorButton = driver.findElement(By.xpath("/html/body/div/div/main/div[2]/table/tbody/tr[1]/td[5]/button"));
 
-        if (statusMsg.getText() == "Active") {
-            activeOwnerButton.click();
+        if (statusMsg.getText().equals("Banned")){
+            statusModeratorButton.click();
             statusMsg = driver.findElement(By.xpath("/html/body/div/div/main/div[2]/table/tbody/tr[1]/td[4]/p"));
         }
 
-        String expectedActivityMsg = "Banned";
+        String expectedActivityMsg = "Active";
         Assert.assertEquals(expectedActivityMsg, statusMsg.getText());
 
         driver.close();
     }
 }
->>>>>>> develop:src/test/java/com/easyrestaurant/tests/AsAdmin_ICan_BanOwner.java
